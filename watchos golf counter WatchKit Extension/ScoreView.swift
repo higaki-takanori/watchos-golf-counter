b81ccf0fc51c8b1ba33b0a9abf558d4fd675f647
higaki-takanori <garoumare@gmail.com>
2022-01-12 23:57:16 +0900

    create score view

---
+++ watchos golf counter WatchKit Extension/ScoreView.swift
@@ -0,0 +1,32 @@
+//
+//  ScoreView.swift
+//  watchos golf counter WatchKit Extension
+//
+//  Created by 檜垣貴仁 on 2022/01/12.
+//
+
+import SwiftUI
+
+struct ScoreView: View {
+    
+    @StateObject var Scores = GlobalScore()
+    
+    var body: some View {
+        List {
+            Text("Total    \(Scores.Score.reduce(0, +)) ( \(Scores.Putter.reduce(0, +)) )")
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.001, saturation: 1.0, brightness: 1.0))
+            
+            ForEach(0..<18) { (row: Int) in
+                Text("\(row+1)H  PAR\(Scores.ParNo[row])  \(Scores.Score[row]) ( \(Scores.Putter[row]) )")
+                
+            }
+        }
+    }
+}
+
+struct ScoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreView()
+    }
+}
